1838d0fbcabb8297bff035ce76e8987a9f7181b5
Your Name <andrijbulgakov5@gmail.com>
2020-04-26 18:26:24 +0300

    another commit

--- src/js/create-items.js
+++ src/js/create-items.js
@@ -2,6 +2,6 @@ import itemTemplate from '../templates/gallery-items.hbs'
 import products from '../menu.json'
 
 const markup = itemTemplate(products)
-// console.log(markup)
+
 
 export default markup
